cd19af0e02d313c33c2a947e24369c6b9e77e3c9
SJKed <simonjurstedtskolan@gmail.com>
2022-02-02 14:52:55 +0100

    Readinglist styling and removal functionality

--- src/views/reading-list.vue
+++ src/views/reading-list.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <li v-for="book in books" :key="book.id">
-      {{ book.Title }}
-    </li>
+    <div class="readingList">
+      <ul>
+        <h1>Reading List</h1>
+        <li v-for="book in books" :key="book.id">
+          <p>{{ book.Title }} by {{ book.Author }} <button @click="removeTargetBook(book)">remove</button></p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -26,8 +31,41 @@ export default {
     books() {
       return this.library.filter((book) => this.list.includes(book.id));
     },
-  }
+  },
+  methods: {
+    removeTargetBook(book) {
+      let bookIds = JSON.parse(localStorage.getItem("bookIds"));
+      let index = bookIds.indexOf(book.id);
+      bookIds.splice(index, 1);
+      localStorage.setItem("bookIds", JSON.stringify(bookIds));
+      location.reload();
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.readingList {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 30vw;
+  height: 100%;
+  background-color: darkslategray;
+
+  h1 {
+    color: white;
+  }
+
+  p {
+    color: white;
+    border-bottom: 1px solid white;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+}
+</style>
