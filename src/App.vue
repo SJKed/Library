e26dd9cecb62dbd439a834fa75cc4fc2e1b6547b
SJKed <simonjurstedtskolan@gmail.com>
2022-02-04 09:16:11 +0100

    added vuex

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-link :books="readingList"  to="/reading-list">Readig List</router-link>
+    <router-link to="/home"> Home Page </router-link>
     <router-view />
   </div>
 </template>
